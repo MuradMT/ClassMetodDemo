b01dbd9d44187ba8a85a720519cc01e4ff74be3b
Murad Mammadzade <mammadzade03@gmail.com>
2022-07-03 23:29:38 +0400

    First change

--- ClassMetodDemo/Program.cs
+++ ClassMetodDemo/Program.cs
@@ -1,5 +1,26 @@
 ﻿namespace ClassMetodDemo
 {
+    class Musteri
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+    class MusteriManager
+    {
+        public void MusteriAdd(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi:"+musteri.ID);
+        }
+        public void MusteriDelete(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi:"+musteri.ID);
+        }
+        public void MusteriList(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Bilgileri:" + musteri.ID+" "+musteri.Name+" "+musteri.Surname);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
